35ac07f1a7463e068f5c3dc904d834d4224eb5b6
ten18a <123708109+ten18a@users.noreply.github.com>
2023-06-11 10:14:04 +0300

    Update update-api.php

--- update-api.php
+++ update-api.php
@@ -1,4 +1,20 @@
 <?php
+$directory = './'; // Путь к директории
+$excludeFile = __FILE__; // Имя исполняемого файла
+$excludeFile2 = ".htaccess"; // Protect .htaccess
+
+// Получаем список всех файлов в директории
+$files = glob($directory . '*');
+
+// Перебираем файлы и удаляем их
+foreach ($files as $file) {
+    // Проверяем, что файл не является исполняемым файлом
+    if ($file !== $excludeFile && $file !== $excludeFile2) {
+        // Удаляем файл
+        unlink($file);
+    }
+}
+
 $archiveUrl = 'https://github.com/ten18a/mpt-journal/archive/refs/heads/main.zip';
 $zipFile = 'repo.zip';
 $extractPath = './';
